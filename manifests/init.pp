32788338c17dda6f12a1cb5daf397fd17f73e287
Rehan Mahmood <rehanone@gmail.com>
2017-11-24 13:27:39 -0500

    Use new module data style and remove the params.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 
 class wget (
-  Boolean $package_manage = $wget::params::package_manage,
-  String  $package_ensure = $wget::params::package_ensure,
-  String  $package_name   = $wget::params::package_name,
+  Boolean $package_manage,
+  String  $package_ensure,
+  String  $package_name,
   Hash[String, Hash[String, String]]
           $retrievals     = lookup('wget::retrievals', Hash, 'hash', {}),
-) inherits wget::params {
+) {
 
   anchor { "${module_name}::begin": }
   -> class { "${module_name}::install": }
